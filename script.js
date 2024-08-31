be34fc9ce245473b5d997a961f3d42a1e6acabc3
douglas <douglas@estudo.com>
2024-08-31 16:56:02 -0300

    git inicial

---
+++ script.js
@@ -0,0 +1,62 @@
+$(document).ready(function () {
+    // Carrossel Slick
+    $('#carousel').slick({
+        autoplay: true,
+        autoplaySpeed: 2000,
+        dots: false,
+        arrows: false
+    })
+
+    $('#form-main').on('submit', function (event) {
+        event.preventDefault()
+    })
+
+
+    $('.menu-hamburger').on('click', function () {
+        $('#navbar').animate({
+            width: 'toggle'
+        }, 300)
+    })
+
+    $('#menu-close').on('click', function () {
+        $('#navbar').animate({
+            width: 'toggle'
+        }, 300)
+    })
+
+    $('.responsive').slick({
+        dots: true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            /*{
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }*/
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+        ]
+    });
+})
